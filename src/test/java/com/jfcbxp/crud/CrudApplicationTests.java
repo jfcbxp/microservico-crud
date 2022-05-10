78450cad1c67e45221b5f886df7e638fe64cff6c
Joao Felipe <jfcbxp@gmail.com>
2022-05-10 20:52:19 -0300

    projeto iniciado

---
+++ src/test/java/com/jfcbxp/crud/CrudApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jfcbxp.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void main() {
+		CrudApplication.main(new String[] {});
+	}
+
+}
