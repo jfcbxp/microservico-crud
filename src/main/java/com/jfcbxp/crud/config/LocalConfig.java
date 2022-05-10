c76ead8ad54cc2882fae0b4c47be4898a1a850a7
Joao Felipe <jfcbxp@gmail.com>
2022-05-10 20:54:01 -0300

    configuração h2 database

---
+++ src/main/java/com/jfcbxp/crud/config/LocalConfig.java
@@ -0,0 +1,11 @@
+package com.jfcbxp.crud.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+
+}
