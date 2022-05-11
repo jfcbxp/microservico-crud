709e77c16e2e46988896f99ae7afdc9ca04e5326
Joao Felipe <jfcbxp@gmail.com>
2022-05-11 13:07:45 -0300

    impl interface produto service findById,update,delete

--- src/main/java/com/jfcbxp/crud/service/impl/ProdutoServiceImpl.java
+++ src/main/java/com/jfcbxp/crud/service/impl/ProdutoServiceImpl.java
@@ -4,15 +4,18 @@ import com.jfcbxp.crud.domain.Produto;
 import com.jfcbxp.crud.domain.dto.ProdutoDTO;
 import com.jfcbxp.crud.repository.ProdutoRepository;
 import com.jfcbxp.crud.service.ProdutoService;
+import com.jfcbxp.crud.service.exception.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProdutoServiceImpl implements ProdutoService {
 
+    public static final String OBJETO_NAO_ENCONTRADO_POR_ESSE_ID = "Objeto não encontrado por esse id";
     @Autowired
     private ProdutoRepository repository;
 
@@ -20,13 +23,29 @@ public class ProdutoServiceImpl implements ProdutoService {
     private ModelMapper mapper;
 
     @Override
-    public Produto createProduto(ProdutoDTO produto) {
-        return repository.save(mapper.map(produto,Produto.class));
+    public Produto createProduto(ProdutoDTO produtoDTO) {
+        return repository.save(mapper.map(produtoDTO,Produto.class));
     }
 
     @Override
     public List<Produto> findAllProduto() {
         return repository.findAll();
+    }
+
+    @Override
+    public Produto findById(Integer id) {
+        Optional<Produto> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO_POR_ESSE_ID));
+    }
+
+    @Override
+    public Produto update(ProdutoDTO produtoDTO) {
+        return repository.save(findById(produtoDTO.getId()));
+    }
+
+    @Override
+    public void delete(Integer id) {
+        repository.deleteById(findById(id).getId());
     }
 
 
