92dbcf5a01598c59a0eaf19715910c63e121ca98
Joao Felipe <jfcbxp@gmail.com>
2022-05-10 21:50:49 -0300

    interface produto service findAllProduto

--- src/main/java/com/jfcbxp/crud/service/ProdutoService.java
+++ src/main/java/com/jfcbxp/crud/service/ProdutoService.java
@@ -3,6 +3,10 @@ package com.jfcbxp.crud.service;
 import com.jfcbxp.crud.domain.Produto;
 import com.jfcbxp.crud.domain.dto.ProdutoDTO;
 
+import java.util.List;
+
 public interface ProdutoService {
     Produto createProduto(ProdutoDTO produto);
+
+    List<Produto> findAllProduto();
 }
