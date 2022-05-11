aa040bb7f5d9c69ea1ac865c3bab44ec1aa23370
Joao Felipe <jfcbxp@gmail.com>
2022-05-10 21:11:30 -0300

    produto service impl createProduto

---
+++ src/main/java/com/jfcbxp/crud/service/impl/ProdutoServiceImpl.java
@@ -0,0 +1,25 @@
+package com.jfcbxp.crud.service.impl;
+
+import com.jfcbxp.crud.domain.Produto;
+import com.jfcbxp.crud.domain.dto.ProdutoDTO;
+import com.jfcbxp.crud.repository.ProdutoRepository;
+import com.jfcbxp.crud.service.ProdutoService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProdutoServiceImpl implements ProdutoService {
+
+    @Autowired
+    private ProdutoRepository repository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Override
+    public Produto createProduto(ProdutoDTO produto) {
+        return repository.save(mapper.map(produto,Produto.class));
+    }
+
+}
