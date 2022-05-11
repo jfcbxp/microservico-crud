afdab711bd8b4f34197598dd9c581ca9ffbeab0c
Joao Felipe <jfcbxp@gmail.com>
2022-05-10 21:12:41 -0300

    padronização ProdutoDTO para o modelmapper

--- src/main/java/com/jfcbxp/crud/domain/dto/ProdutoDTO.java
+++ src/main/java/com/jfcbxp/crud/domain/dto/ProdutoDTO.java
@@ -1,12 +1,10 @@
 package com.jfcbxp.crud.domain.dto;
 
-import com.jfcbxp.crud.domain.Produto;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.modelmapper.ModelMapper;
 
 import java.math.BigDecimal;
 
@@ -20,8 +18,4 @@ public class ProdutoDTO {
     private String name;
     private BigDecimal estoque;
     private BigDecimal preco;
-
-    public static ProdutoDTO create(Produto produto) {
-        return new ModelMapper().map(produto, ProdutoDTO.class);
-    }
 }
