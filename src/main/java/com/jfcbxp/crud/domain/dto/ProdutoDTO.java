a213360fb891416d1758cf01e49ab832aaaff53f
Joao Felipe <jfcbxp@gmail.com>
2022-05-11 14:11:06 -0300

    adicionamento Representation Model para classe ProdutoDTO

--- src/main/java/com/jfcbxp/crud/domain/dto/ProdutoDTO.java
+++ src/main/java/com/jfcbxp/crud/domain/dto/ProdutoDTO.java
@@ -5,15 +5,21 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.hateoas.RepresentationModel;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@EqualsAndHashCode
-public class ProdutoDTO {
+@EqualsAndHashCode(callSuper = false)
+public class ProdutoDTO extends RepresentationModel<ProdutoDTO> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 7069290981939274043L;
+
     private Integer id;
     private String name;
     private BigDecimal estoque;
