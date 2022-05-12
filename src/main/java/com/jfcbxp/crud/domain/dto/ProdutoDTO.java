f8cfb820fa0e455c00c97824f2ed6dfe13f44019
Joao Felipe <jfcbxp@gmail.com>
2022-05-11 21:55:48 -0300

    validações na classe produto dto

--- src/main/java/com/jfcbxp/crud/domain/dto/ProdutoDTO.java
+++ src/main/java/com/jfcbxp/crud/domain/dto/ProdutoDTO.java
@@ -1,5 +1,7 @@
 package com.jfcbxp.crud.domain.dto;
 
+import com.jfcbxp.crud.validation.CreateProdutoValidation;
+import com.jfcbxp.crud.validation.UpdateProdutoValidation;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -7,6 +9,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.hateoas.RepresentationModel;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
 import java.io.Serial;
 import java.io.Serializable;
 import java.math.BigDecimal;
@@ -20,6 +24,8 @@ public class ProdutoDTO extends RepresentationModel<ProdutoDTO> implements Seria
     @Serial
     private static final long serialVersionUID = 7069290981939274043L;
 
+    @NotNull(groups = UpdateProdutoValidation.class,message = "id não pode ser nulo")
+    @Null(groups = CreateProdutoValidation.class,message = "id deve ser nulo")
     private Integer id;
     private String name;
     private BigDecimal estoque;
